032e54d2abebd15c7146c2f7ed2211fb983fc13f
tirec <albertsson.therese@gmail.com>
2016-09-08 11:45:27 +0200

    Linking quiz with questions

--- quiz/views.py
+++ quiz/views.py
@@ -10,8 +10,16 @@ def quiz(request, quiz_number):
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/quiz.html", context)
-def question(request):
-	return render(request, "quiz/question.html")
+def question(request, quiz_number, question_number):
+	context = {
+		"question_number": question_number,
+	    "question": "Hur många bultar har ölandsbron?",
+		"answer1": "12",
+	   	"answer2": "66 400",
+	    "answer3": "7 428 954",
+	    "quiz_number": quiz_number,
+	}
+	return render(request, "quiz/question.html", context)
 def completed(request):
 	return render(request, "quiz/completed.html")
 
