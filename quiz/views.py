52b86acd51e45beecff904f6f8e107298258fcad
tirec <albertsson.therese@gmail.com>
2016-09-08 10:55:45 +0200

    created views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+def startpage(request):
+return render(request, "quiz/startpage.html")
+def quiz(request):
+return render(request, "quiz/quiz.html")
+def question(request):
+return render(request, "quiz/question.html")
+def completed(request):
+return render(request, "quiz/completed.html")
+from django.shortcuts import render
 
 # Create your views here.
