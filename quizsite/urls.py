47e745da085648314129569061c68164bacd5c38
tirec <albertsson.therese@gmail.com>
2016-09-08 12:30:34 +0200

    Link all pages

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from quiz import views
 urlpatterns = [
-	url(r"^$", views.startpage),
-	url(r"^quiz/([0-9]+)/$", views.quiz),
-	url(r"^quiz/([0-9])+/question/([0-9])+/$", views.question),
-	url(r"^quiz/([0-9])+/completed/$", views.completed),
+	url(r"^$", views.startpage, name="start_page"),
+	url(r"^quiz/([0-9]+)/$", views.quiz, name="quiz_page"),
+	url(r"^quiz/([0-9])+/question/([0-9])+/$", views.question, name="question_page"),
+	url(r"^quiz/([0-9])+/completed/$", views.completed, name="completed_page"),
 	]
