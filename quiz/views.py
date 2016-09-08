d47f4283d4d12a916006014f7d6eff494024ea7e
tirec <albertsson.therese@gmail.com>
2016-09-08 11:40:20 +0200

    Quiz numbers

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
 def startpage(request):
-	return render(request, "quiz/startpage.html")
-def quiz(request):
-	return render(request, "quiz/quiz.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/startpage.html", context)
+def quiz(request, quiz_number):
+	context = {
+		"quiz": quizzes[int(quiz_number) - 1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 def question(request):
 	return render(request, "quiz/question.html")
 def completed(request):
 	return render(request, "quiz/completed.html")
+
+quizzes = [
+	{
+		"quiz_number": 1,
+   		"name": "Klassiska böcker",
+	   	"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+   	   	"name": "Största fotbollslagen",
+	   	"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+   	    	"name": "Världens mest kända hackare",
+	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+]
 
 # Create your views here.
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from quiz import views
 urlpatterns = [
 	url(r"^$", views.startpage),
-	url(r"^quiz/[0-9]+/$", views.quiz),
+	url(r"^quiz/([0-9]+)/$", views.quiz),
 	url(r"^quiz/[0-9]+/question/[0-9]+/$", views.question),
 	url(r"^quiz/[0-9]+/completed/$", views.completed),
 	]
