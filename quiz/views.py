e10e557ae54c0bcee000abbfb1ad32bfb1eaffae
tirec <albertsson.therese@gmail.com>
2016-09-09 14:34:19 +0200

    heroku setup

--- quiz/views.py
+++ quiz/views.py
@@ -27,6 +27,7 @@ def question(request, quiz_number, question_number):
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
+
 def answer(request, quiz_number, question_number):
     saved_answers = request.session.get(quiz_number, {})
     answer = int(request.POST["answer"])
@@ -39,6 +40,7 @@ def answer(request, quiz_number, question_number):
     	return redirect("completed_page", quiz_number)
     else:
     	return redirect("question_page", quiz_number, question_number + 1)
+
 def completed(request, quiz_number):
 	quiz = Quiz.objects.get(quiz_number=quiz_number)
 	questions = quiz.questions.all()
@@ -46,11 +48,11 @@ def completed(request, quiz_number):
 	num_correct_answers = 0
 	for question_number, answer in saved_answers.items():
 		correct_answer = questions[int(question_number) - 1].correct
-	if correct_answer == answer:
-		num_correct_answers += 1
+		if correct_answer == answer:
+			num_correct_answers += 1
 	context = {
-    "correct": num_correct_answers,
-    "total": questions.count(),
+		"correct": num_correct_answers,
+		"total": questions.count(),
 	}
 	return render(request, "quiz/completed.html", context)
 
--- quizsite/settings.py
+++ quizsite/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
+'db.sqlite3'))
+    )
 }
+
 
 
 # Password validation
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATIC_ROOT = 'staticfiles'
