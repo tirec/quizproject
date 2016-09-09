ac91b224ec48df38a63f8b2831aaa156ef24db66
tirec <albertsson.therese@gmail.com>
2016-09-09 12:14:32 +0200

    views

--- quiz/views.py
+++ quiz/views.py
@@ -40,9 +40,17 @@ def answer(request, quiz_number, question_number):
     else:
     	return redirect("question_page", quiz_number, question_number + 1)
 def completed(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	saved_answers = request.session.get(quiz_number, {})
+	num_correct_answers = 0
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number) - 1].correct
+	if correct_answer == answer:
+		num_correct_answers += 1
 	context = {
-		"correct": 12,
-		"total": 20,
-		"quiz_number": quiz_number,
+    "correct": num_correct_answers,
+    "total": questions.count(),
 	}
 	return render(request, "quiz/completed.html", context)
+
